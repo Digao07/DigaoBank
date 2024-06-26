90a24ac974eb99dcef6f30d018e1cd4eee8d251c
Rodrigo Barros <126627525+Digao07@users.noreply.github.com>
2024-06-26 16:44:40 -0300

    Update login.test.tsx

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,12 +1,12 @@
-import { login } from "./login"
+// src/services/login.test.ts
+import { login } from './login';
+
+// Mocking the alert function
+global.alert = jest.fn();
 
 describe('login', () => {
-
-    const mockAlert = jest.fn()
-    window.alert = mockAlert
-
-    it('Deve exibir um alert com boas vindas', () => {
-        login()
-        expect(mockAlert).toHaveBeenCalledWith('Bem vinda!')
-    })
-})
+  it('deve exibir uma mensagem de boas-vindas ao ser chamado', () => {
+    login();
+    expect(global.alert).toHaveBeenCalledWith('Bem-vindo ao Digao Bank!');
+  });
+});
