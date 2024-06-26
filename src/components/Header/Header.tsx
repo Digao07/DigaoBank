c586ccd14d9caf54c166bd879f05370c150a95d4
Rodrigo Barros <126627525+Digao07@users.noreply.github.com>
2024-06-26 16:47:33 -0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
-import './Header.css'
+// src/components/Header.tsx
+import React from 'react';
+import { Center, Text } from '@chakra-ui/react';
+import './Header.css';
 
-export const Header  = () => {
-  return(
-    <div className='header'>
-      Dio Bank
-    </div>
-  )
-}
+export const Header: React.FC = () => {
+    return (
+        <Center className='header' padding='10px' backgroundColor='#FFFFFF' borderRadius='10px' marginBottom='20px'>
+            <Text fontSize='3xl' fontWeight='bold'>
+                Digao Bank
+            </Text>
+        </Center>
+    );
+};
